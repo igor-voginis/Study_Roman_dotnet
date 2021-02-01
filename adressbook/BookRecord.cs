e8ab2f0ad878e9366d61f722506d18d926bc84c6
igor <voginis>
2021-02-01 14:24:10 +0300

    new AdressBook Class

---
+++ adressbook/BookRecord.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace adressbook
+{
+    class BookRecord
+    {
+        public int _number;
+        public string _name;
+        public string _country;
+
+        public BookRecord (int number, string name, string country)
+        {
+            _number = number;
+            _name = name;
+            _country = country;
+        }
+        public BookRecord(string name, string country) 
+        {
+            _name = name;
+            _country = country;
+        }
+        public BookRecord() { }
+
+        public void GetInfo()
+        {
+            Console.WriteLine("№ " + _number + " Имя " + _name + " Страна " + _country);
+        }
+
+        public void Add()
+        {
+            
+            Console.WriteLine("Добавьте новое имя");
+            _name = Console.ReadLine();
+            Console.WriteLine("Добавьте страну для нового имени");
+            _country = Console.ReadLine();
+        }
+    }
+            
+}
--- adressbook/Program.cs
+++ adressbook/Program.cs
@@ -7,33 +7,53 @@ namespace adressbook
     {
         static void Main(string[] args)
         {
+
+
+            BookRecord bookRecord = new BookRecord(1, "kate", "Беларусь");
+            BookRecord bookRecord1 = new BookRecord(2, "vasya", "Мексика");
+            BookRecord bookRecord2 = new BookRecord(3, "igor", "Израиль");
+
+                 
            
-            string[] name = new string[] { "kate", "vasya", "igor"};
-            int[] number = new int[] { 1, 2, 3 };
-            string[] country = new string[] {"Беларусь", "Мексика", "Израиль" };
-            
-            for (int i = 3; i < 5; i++)
-            {
-                Console.WriteLine("введите имя");
-                string nextName = Console.ReadLine();
-                name = name.Concat(new string[] {$"{nextName}"}).ToArray();
+            bookRecord.GetInfo();
+            bookRecord1.GetInfo();
+            bookRecord2.GetInfo();
 
-                Console.WriteLine("введите номер");
-                int nextNumber = Convert.ToInt32(Console.ReadLine());
-                number = number.Concat(new int[] {nextNumber}).ToArray();
+            BookRecord bookRecord3 = new BookRecord();
+            bookRecord3.Add();
 
-                Console.WriteLine("введите страну");
-                string nextCountru = Console.ReadLine();
-                country = country.Concat(new string[] { $"{nextCountru}" }).ToArray();
+            bookRecord3.GetInfo();
 
-            }
-            
 
-            for (int i = 0; i < name.Length; i++)
-            {
-                Console.WriteLine($"Имя: {name[i]} Номер: {number[i]} Страна: {country[i]}");
-            }
-            
+
+
+
+            //string[] name = new string[] { "kate", "vasya", "igor"};
+            //int[] number = new int[] { 1, 2, 3 };
+            //string[] country = new string[] {"Беларусь", "Мексика", "Израиль" };
+
+            //for (int i = 3; i < 5; i++)
+            //{
+            //    Console.WriteLine("введите имя");
+            //    string nextName = Console.ReadLine();
+            //    name = name.Concat(new string[] {$"{nextName}"}).ToArray();
+
+            //    Console.WriteLine("введите номер");
+            //    int nextNumber = Convert.ToInt32(Console.ReadLine());
+            //    number = number.Concat(new int[] {nextNumber}).ToArray();
+
+            //    Console.WriteLine("введите страну");
+            //    string nextCountru = Console.ReadLine();
+            //    country = country.Concat(new string[] { $"{nextCountru}" }).ToArray();
+
+            //}
+
+
+            //for (int i = 0; i < name.Length; i++)
+            //{
+            //    Console.WriteLine($"Имя: {name[i]} Номер: {number[i]} Страна: {country[i]}");
+            //}
+
 
         }
     }
